d40630386652cfaf824949288097b3498f46c2db
FANATFANATA <FANATFANATA@users.noreply.github.com>
2026-09-23 10:45:08 +0300

    test: force byok off in conftest so local .env cannot flip test mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import os
 
 os.environ["DANYAPI_HUMAN_DELAY_MIN"] = "0"
 os.environ["DANYAPI_HUMAN_DELAY_MAX"] = "0"
+os.environ["BYOK"] = "0"
+os.environ["BYOK_MODE"] = "0"
+os.environ["DANYAPI_BYOK_MODE"] = "0"
 for _key in (
     "DEEPSEEK_TOKENS",
     "QWEN_TOKENS",
